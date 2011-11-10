b1a6e7db6772f0af8f9a975050b8651e8b667940
Raúl Acuña <reacuna@gmail.com>
2011-11-10 20:53:11 +0000

    Add small files to play

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var TestApp = {};
+
+TestApp.resizeContent = function(element) {
+    $(element).height(100);
+};
+
+TestApp.init = function() {
+    
+};
+
+jQuery(document).ready(function(){
+    TestApp.init();
+    });
